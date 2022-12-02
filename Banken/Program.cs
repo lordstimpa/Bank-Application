2688deb22e81c20e49f249a28501461ed6ad5b12
Steven Dalfall <steven.dalfall@live.se>
2022-12-02 17:04:47 +0100

    added a method to transfer money between accounts

--- Banken/Program.cs
+++ Banken/Program.cs
@@ -3,50 +3,92 @@ class Program
 {
     static void Main(string[] args)
     {
-        bool repeat = true;
+        bool repeatMain = true;
+        UserAccount? currentUser;
 
-        List<Account> accountList = new List<Account>();
+        List<UserAccount> userAccountList = new List<UserAccount>();
 
+        // Hårdkodad användare #1
+        UserAccount user0 = new UserAccount("Greger", "123");
+        user0.SavingsAccounts.Add(new SavingsAccount(5000));
+        user0.PayrollAccounts.Add(new PayrollAccount(1000));
+        userAccountList.Add(user0);
+
+        // Hårdkodad användare #2
+        UserAccount user1 = new UserAccount("Kristian", "abc");
+        user1.SavingsAccounts.Add(new SavingsAccount(10000));
+        user1.PayrollAccounts.Add(new PayrollAccount(2000));
+        userAccountList.Add(user1);
+
+        // Hårdkodad användare #3
+        UserAccount user2 = new UserAccount("Erik", "fabel2");
+        user2.SavingsAccounts.Add(new SavingsAccount(20000));
+        user2.PayrollAccounts.Add(new PayrollAccount(4000));
+        userAccountList.Add(user2);
+
+        // Hårdkodad användare #4
+        UserAccount user3 = new UserAccount("Anton", "anteh52");
+        user3.SavingsAccounts.Add(new SavingsAccount(40000));
+        user3.PayrollAccounts.Add(new PayrollAccount(8000));
+        userAccountList.Add(user3);
+
+        // Hårdkodad användare #5
+        UserAccount user4 = new UserAccount("Mohammed", "98hamed89");
+        user4.SavingsAccounts.Add(new SavingsAccount(80000));
+        user4.PayrollAccounts.Add(new PayrollAccount(16000));
+        userAccountList.Add(user4);
+
+        // do-while STARTMENY
         do
         {
             Console.Clear();
 
-            int optionStart = printStartMenu();
+            int optionStart = PrintStartMenu();
 
+            // switch STARTMENY
             switch (optionStart)
             {
                 case 1:
-                    bool success = logIn(accountList);
+                    UserAccount success = LogIn(userAccountList);
 
-                    if (success == true)
+                    if (success is UserAccount)
                     {
                         bool repeatUser = true;
+                        currentUser = success;
 
+                        // do-while ANVÄNDARMENY
                         do
                         {
                             Console.Clear();
 
-                            int optionUser = printUserMenu();
+                            int optionUser = PrintUserMenu();
 
+                            // switch ANVÄNDARMENY
                             switch (optionUser)
                             {
                                 case 1:
                                     Console.Clear();
-                                    foreach (var account in accountList)
+
+                                    foreach (SavingsAccount saving in currentUser.SavingsAccounts)
                                     {
-                                        Console.WriteLine("Användarnamn: {0}\nSaldo: {1}", account.Username, account.Balance) ;
+                                        Console.WriteLine("Konto: {0}\nTotal saldo: {1}\n", saving.Accountname, saving.Balance);
                                     }
+
+                                    foreach (PayrollAccount payroll in currentUser.PayrollAccounts)
+                                    {
+                                        Console.WriteLine("Konto: {0}\nTotal saldo: {1}", payroll.Accountname, payroll.Balance);
+                                    }
+
                                     Console.Write("\nTryck enter för att komma till användarmenyn.");
                                     Console.ReadLine();
-
                                     break;
-
-                                case 2:
-
+                                        
+                                case 2:                                   
+                                    TransferMoney(currentUser);
                                     break;
 
                                 case 3:
-
+                                    WithdrawMoney(currentUser);
                                     break;
 
                                 case 4:
@@ -63,28 +105,28 @@ class Program
                     else
                     {
                         Console.WriteLine("Du har inga försök kvar.");
-                        Console.Write("Tryck enter för att komma till huvudmenyn.");
+                        Console.Write("\nTryck enter för att komma till huvudmenyn.");
                         Console.ReadLine();
                     }
                     break;
 
                 case 2:
-                    accountList = createAccount(accountList);
+                    userAccountList.Add(CreateAccount());
                     break;
 
                 case 3:
                     Console.WriteLine("\nTack för att du använt banken, Välkommen åter.");
-                    repeat = false;
+                    repeatMain = false;
                     break;
 
                 default:
                     Console.WriteLine("\nEtt fel har uppstått, kontakta utvecklaren.");
                     break;
             }
-        } while (repeat);
+        } while (repeatMain);
     }
 
-    static int printStartMenu()
+    static int PrintStartMenu()
     {
         bool success;
         int result;
@@ -129,7 +171,7 @@ class Program
         return result;
     }
     
-    static int printUserMenu()
+    static int PrintUserMenu()
     {
         bool success;
         int result;
@@ -171,85 +213,190 @@ class Program
         return result;
     }
 
-    static List<Account> createAccount(List<Account> accountList)
+    static UserAccount CreateAccount()
     {
-        Console.Clear();
-        Console.Write("Användarnamn: ");
-        string username = Console.ReadLine();
+        bool repeat = true;
+        string username, password;
 
-        Console.Write("\nLösenord: ");
-        string password = Console.ReadLine();
+        do
+        {
+            Console.Clear();
+            Console.Write("Användarnamn: ");
+            username = Console.ReadLine();
 
-        accountList.Add(new Account(username, password));
+            Console.Write("\nLösenord: ");
+            password = Console.ReadLine();
 
-        Console.WriteLine("\nEn ny användare har skapats!");
-        Console.Write("Tryck enter för att komma till huvudmenyn.");
-        Console.ReadLine();
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                Console.WriteLine("\nDu måste ange ett användarnamn och lösenord.");
+                Console.Write("Tryck enter för att försöka igen.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("\nEn ny användare har skapats!");
+                Console.Write("Tryck enter för att komma till huvudmenyn.");
+                Console.ReadLine();
+                repeat = false;
+            }
+        } while (repeat == true);
 
-        return accountList;
+        return new UserAccount(username, password);
     }
 
-    static bool logIn(List<Account> accountList)
+    static UserAccount? LogIn(List<UserAccount> userAccountList)
     {
         int attempts = 5;
-        bool success = false;
 
         Console.Clear();
-
-        if (accountList.Count() == 0)
-        {
-            attempts = 0;
-            success = false;
-        }
 
         while (attempts > 0)
         {
             Console.Write("Användarnamn: ");
-            string userName = Console.ReadLine();
+            string username = Console.ReadLine();
 
             Console.Write("\nLösenord: ");
-            string passWord = Console.ReadLine();
+            string password = Console.ReadLine();
 
-            foreach (var account in accountList)
+            foreach (UserAccount account in userAccountList)
             {
-                if (userName == account.Username && passWord == account.Password)
+                if (username == account.Username && password == account.Password)
                 {
-                    attempts = 0;
-                    success = true;
-                    break;
+                    return account;
                 }
-                else
-                {
-                    attempts--;
-                    success = false;
-                    Console.Clear();
-                    Console.WriteLine("Detta konto är inte registrerat.");
-                    Console.WriteLine(attempts + " försök kvar.");
-                    break;
-                }
+
             }
+            attempts--;
+            Console.Clear();
+            Console.WriteLine("Detta konto är inte registrerat.");
+            Console.WriteLine(">" + attempts + "< försök kvar.\n");
         }
-        return success;
+        return null;
     }
 
-    static void transferMoney()
+    static void TransferMoney(UserAccount currentUser)
     {
+        bool success;
+        string option, amount;
 
+        do
+        {
+            int iterations = 0;
+
+            Console.Clear();
+            Console.WriteLine("Lista med konton för att hantera överföring.");
+
+            foreach (SavingsAccount saving in currentUser.SavingsAccounts)
+            {
+                iterations++;
+                Console.WriteLine(iterations + ". {0}", saving.Accountname);
+            }
+
+            foreach (PayrollAccount payroll in currentUser.PayrollAccounts)
+            {
+                iterations++;
+                Console.WriteLine(iterations + ". {0}", payroll.Accountname);
+            }
+
+            Console.WriteLine("\nVänligen ange ett alternativ: ");
+            Console.Write("--->");
+            option = Console.ReadLine();
+
+            success = float.TryParse(option, out float result);
+
+            switch (result)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine("Ange belopp att överföra till lönekonto.");
+                    Console.Write("---> ");
+                    amount = Console.ReadLine();
+
+                    success = float.TryParse(amount, out result);
+
+                    if (success)
+                    {
+                        Console.Clear();
+                        foreach (SavingsAccount savingsAccount in currentUser.SavingsAccounts)
+                        {
+                            savingsAccount.Balance -= result;
+                            Console.WriteLine("Konto: {0}\nTotal saldo: {1}\n", savingsAccount.Accountname, savingsAccount.Balance);
+                        }
+                        foreach (PayrollAccount payrollAccount in currentUser.PayrollAccounts)
+                        {
+                            payrollAccount.Balance += result;
+                            Console.WriteLine("Konto: {0}\nTotal saldo: {1}\n", payrollAccount.Accountname, payrollAccount.Balance);
+                        }
+                        Console.Write("\nTryck enter för att komma till användarmenyn.");
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Vänligen ange ett giltigt belopp.");
+                        Console.Write("\nTryck enter för att gå tillbaka.");
+                        Console.ReadLine();
+                    }
+                    break;
+
+                case 2:
+                    Console.WriteLine("Ange belopp att överföra till sparkonto.");
+                    Console.Write("---> ");
+                    amount = Console.ReadLine();
+
+                    success = float.TryParse(amount, out result);
+
+                    if (success)
+                    {
+                        Console.Clear();
+                        foreach (SavingsAccount savingsAccount in currentUser.SavingsAccounts)
+                        {
+                            savingsAccount.Balance += result;
+                            Console.WriteLine("Konto: {0}\nTotal saldo: {1}\n", savingsAccount.Accountname, savingsAccount.Balance);
+                        }
+                        foreach (PayrollAccount payrollAccount in currentUser.PayrollAccounts)
+                        {
+                            payrollAccount.Balance -= result;
+                            Console.WriteLine("Konto: {0}\nTotal saldo: {1}\n", payrollAccount.Accountname, payrollAccount.Balance);
+                        }
+                        Console.Write("\nTryck enter för att komma till användarmenyn.");
+                        Console.ReadLine();
+                        success = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Vänligen ange ett giltigt belopp.");
+                        Console.Write("\nTryck enter för att försöka igen.");
+                        Console.ReadLine();
+                    }
+                    break;
+
+                case 3:
+                    success = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Vänligen ange ett alternativ mellan 1-3.");
+                    break;                     
+            }
+
+        } while (!success);
     }
 
-    static void withdrawMoney()
+    static void WithdrawMoney(UserAccount currentUser)
     {
 
     }
 }
-
-public class Account
+ 
+class UserAccount
 {
-    string _userName;
-    string _passWord;
-    double _balance;
+    private string _userName;
+    private string _passWord;
+    private List<SavingsAccount> _savingsAccounts = new List<SavingsAccount>();
+    private List<PayrollAccount> _payrollAccounts = new List<PayrollAccount>();
 
-    public Account(string username, string password)
+    public UserAccount(string username, string password)
     {
         _userName = username;
         _passWord = password;
@@ -267,10 +414,58 @@ public class Account
         set { _passWord = value; }
     }
 
-    public double Balance
+    public List<SavingsAccount> SavingsAccounts
+    {
+        get { return _savingsAccounts; }
+        set { _savingsAccounts = value; }
+    }
+
+    public List<PayrollAccount> PayrollAccounts
+    {
+        get { return _payrollAccounts; }
+        set { _payrollAccounts = value; }
+    }
+}
+
+class SavingsAccount
+{
+    private string _accountName = "Savings account";
+    private float _balance = 5000;
+
+    public SavingsAccount(float balance)
+    {
+        _balance = balance;
+    }
+    public string Accountname
+    {
+        get { return _accountName; }
+        set { _accountName = value; }
+    }
+    public float Balance
     {
         get { return _balance; }
         set { _balance = value; }
     }
+}
 
+class PayrollAccount
+{
+    private string _accountName = "Payroll account";
+    private float _balance = 1000;
+
+    public PayrollAccount(float balance)
+    {
+        _balance = balance;
+    }
+    public string Accountname
+    {
+        get { return _accountName; }
+        set { _accountName = value; }
+    }
+
+    public float Balance
+    {
+        get { return _balance; }
+        set { _balance = value; }
+    }
 }
